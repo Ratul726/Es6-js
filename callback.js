e25df5a3d24ed8a10381967fc4dfb8e080fa4fc7
Ratul726 <ratulx726492@gmail.com>
2024-02-27 11:34:25 -0800

    second commit

---
+++ callback.js
@@ -0,0 +1,8 @@
+// function greeting (person){
+//     console.log(person)
+// }
+// greeting('galan caca')
+
+const arr = [154,55,20,90,8540]
+const arrat = arr.find(array =>array % 10===0)
+console.log(arrat)
---
+++ common.js
@@ -0,0 +1,2 @@
+const arr = [40,52,6,4]
+console.log(typeof Array.isArray(arr))
